07943aa3fa1c432c710a9dfe5c0ed538aaac7696
dafailyasa <dafailyasa30@gmail.com>
2023-02-21 23:36:44 +0700

    fix name

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const httpStatus = require("http-status");
 const config = require("../config/config");
 const logger = require("../config/logger");
-const ApiError = require("../utils/ApiError");
+const ApiError = require("../utils/apiError");
 
 const errorConverter = (err, req, res, next) => {
   let error = err;
