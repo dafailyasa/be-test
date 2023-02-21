949e326237405dc3dd9de4b19cf6c0bc432032b8
dafailyasa <dafailyasa30@gmail.com>
2023-02-21 23:33:41 +0700

    use env proccess

--- src/config/config.js
+++ src/config/config.js
@@ -4,29 +4,12 @@ const Joi = require("joi");
 
 dotenv.config({ path: path.join(__dirname, "../../.env") });
 
-const envVarsSchema = Joi.object()
-  .keys({
-    NODE_ENV: Joi.string()
-      .valid("production", "development", "test")
-      .required(),
-    PORT: Joi.number().default(3000),
-    MONGODB_URL: Joi.string().required().description("Mongo DB url"),
-  })
-  .unknown();
-
-const { value: envVars, error } = envVarsSchema
-  .prefs({ errors: { label: "key" } })
-  .validate(process.env);
-
-if (error) {
-  throw new Error(`Config validation error: ${error.message}`);
-}
 
 module.exports = {
-  env: envVars.NODE_ENV,
-  port: envVars.PORT,
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
   mongoose: {
-    url: envVars.MONGODB_URL + (envVars.NODE_ENV === "test" ? "-test" : ""),
+    url: process.env.MONGODB_URL + (process.env.NODE_ENV === "test" ? "-test" : ""),
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true,
